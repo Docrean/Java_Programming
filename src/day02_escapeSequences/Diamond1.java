48ddb178122c188a797d65e119743d0e7c25e6ef
Docrean <bverns88@gmail.com>
2023-11-13 19:59:58 -0800

    Day01 practice/tasks complete

---
+++ src/day02_escapeSequences/Diamond1.java
@@ -0,0 +1,15 @@
+package day02_escapeSequences;
+
+public class Diamond1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t         ^    ");
+        System.out.println("\t        / \\    ");
+        System.out.println("\t       /   \\    ");
+        System.out.println("\t      /     \\   ");
+        System.out.println("\t     /       \\   ");
+        System.out.println("\t     ---------    ");
+
+    }
+}
