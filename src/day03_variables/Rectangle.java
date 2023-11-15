3972e0eaeef420b5a4fefc195134f692aee66f02
DocB <bverns88@gmail.com>
2023-11-15 12:40:35 -0800

    Rectangle1

---
+++ src/day03_variables/Rectangle.java
@@ -0,0 +1,13 @@
+package day03_variables;
+
+public class Rectangle {
+
+    public static void main(String[] args) {
+
+        double width = 3.5;
+        double length = 3.4;
+
+        System.out.println("Area = " + width + " * " + length);
+
+    }
+}
