7852a157fabdf10dcf0ba30e8aeb8d3ddf7f6e50
DocB <bverns88@gmail.com>
2023-11-15 01:59:45 -0800

    Day02_complete

---
+++ src/day02_escapeSequences/Face.java
@@ -0,0 +1,17 @@
+package day02_escapeSequences;
+
+public class Face {
+
+    public static void main(String[] args) {
+
+        System.out.println("\t   +\"\"\"\"\"+   " );
+        System.out.println("\t  [|   o   o  |]   ");
+        System.out.println("\t   |     ^    |    ");
+        System.out.println("     |   | _ |  |     ");
+        System.out.println("     +----------+    ");
+
+
+
+
+    }
+}
