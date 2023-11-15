d848626c50a7aff2ed3c690192d9b05ef08976f2
DocB <bverns88@gmail.com>
2023-11-15 12:33:38 -0800

    StudentInfo2(DocB redo) final

---
+++ src/day03_variables/StudentInfo2.java
@@ -0,0 +1,27 @@
+package day03_variables;
+
+public class StudentInfo2 {
+
+    public static void main(String[] args) {
+
+        String student_name = "Self",
+                school_name = "Edison",
+                student_Id = "B1234",
+                grade_level = "5th Grade";
+        int age = 21;
+        char gender = 'F';
+        double gpa = 3.5;
+
+        System.out.println("student_name = " + student_name);
+        System.out.println("school_name = " + school_name);
+        System.out.println("student_Id = " + student_Id);
+        System.out.println("grade_level = " + grade_level);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("gpa = " + gpa);
+
+
+
+
+    }
+}
