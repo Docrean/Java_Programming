fab9e9b1efc16fe5714c7eaf8f047fca16ef3d3a
DocB <bverns88@gmail.com>
2023-11-15 12:21:00 -0800

    EmployeeInfo2(DocB redo)

---
+++ src/day03_variables/EmployeeInfo2.java
@@ -0,0 +1,30 @@
+package day03_variables;
+
+public class EmployeeInfo2 {
+
+    public static void main(String[] args) {
+
+        String Name = "Employee";
+        int age = 21;
+        char gender = 'M';
+        String companyName = "Barns-worth";
+        double employeeId = 1234;
+        String jobTitle = "Cleaner";
+        double salary = 100000;
+        boolean isFullTime = true;
+
+        System.out.println("Name = " + Name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("companyName = " + companyName);
+        System.out.println("employeeId = " + employeeId);
+        System.out.println("jobTitle = " + jobTitle);
+        System.out.println("salary = " + salary);
+        System.out.println("isFullTime = " + isFullTime);
+
+
+
+
+
+    }
+}
