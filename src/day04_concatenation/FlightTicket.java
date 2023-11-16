306f262f61a7c61fd3d58420c067d4fcce8843ae
DocB <bverns88@gmail.com>
2023-11-16 02:44:35 -0800

    FlightTicket

---
+++ src/day04_concatenation/FlightTicket.java
@@ -0,0 +1,29 @@
+package day04_concatenation;
+
+public class FlightTicket{
+
+    public static void main(String[] args) {
+
+
+        String from = "Las Vegas",
+                to = "Mclean";
+
+        double ticketPrice = 425.5;
+
+        System.out.println("From " + from + " to " + to + " is " + ticketPrice);
+
+
+    }
+
+}
+
+/*
+1. Create a class named FlightTicket.java
+2. Declare the following variables:
+        1. from
+        2. to
+        3. ticketPrice
+        3. Use concatenation to print the full ticket information
+
+    Ex: From Las Vegas to McLean is $425.5
+*/
