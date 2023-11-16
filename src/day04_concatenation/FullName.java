10287830bb398d81f40f929ae4eb24fba0c8a2d9
DocB <bverns88@gmail.com>
2023-11-16 02:33:04 -0800

    day04_FullName

---
+++ src/day04_concatenation/FullName.java
@@ -0,0 +1,16 @@
+package day04_concatenation;
+
+public class FullName {
+
+    public static void main(String[] args) {
+
+        String FirstName = "Brandon",
+                lastname = "Vernon";
+
+        System.out.println("Your full name is " + FirstName + " " + lastname);
+
+
+
+
+    }
+}
