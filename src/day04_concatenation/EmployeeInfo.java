4c2cf87ab4dd37c7a77645c4cf019bbbf77c73cf
Docrean <bverns88@gmail.com>
2023-11-17 01:56:57 -0800

    Day04_EmployeeInfo update

---
+++ src/day04_concatenation/EmployeeInfo.java
@@ -0,0 +1,15 @@
+package day04_concatenation;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        String name = "Daniel",
+                company_name = "Google Inc",
+                jobTitle = "Software Developer";
+
+        long age = 28;
+
+
+    }
+}
