7a72f32513bb4ec48a7a5f5571933f1c49a1c17d
DocB <bverns88@gmail.com>
2023-11-17 02:33:12 -0800

    CarInfo final

--- src/day04_concatenation/EmployeeInfo.java
+++ src/day04_concatenation/EmployeeInfo.java
@@ -6,9 +6,15 @@ public class EmployeeInfo {
 
         String name = "Daniel",
                 company_name = "Google Inc",
-                jobTitle = "Software Developer";
+                jobTitle = "Software Developer",
+                gender = "Male";
 
-        long age = 28;
+        long age = 28,
+                salary = 90000;
+
+        System.out.println(name + " is " + age + " years old, " + "gender is " + gender + ".\n"
+                + name + " works at " + company_name + " as a " + jobTitle + ".\n"
+                + name + " makes $" + salary + " per year.");
 
 
     }
