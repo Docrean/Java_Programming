df0f5d7499eac43c6e3ac755b2729ba3cb545055
Docrean <bverns88@gmail.com>
2023-11-17 20:40:30 -0800

    Swapvariable1 save

---
+++ src/day04_concatenation/SwapTowVariables1.java
@@ -0,0 +1,23 @@
+package day04_concatenation;
+
+public class SwapTowVariables1 {
+
+    public static void main(String[] args) {
+
+        /*
+Create a class named SwapTwoVariables1 and declare the following variables:
+            x
+            y
+            z
+    8.1 Write a program that swap variables x & y’ values by using a temporary variable z
+        Ex:
+              x = 10
+              y = 15
+        output:
+                  x = 15
+                  y = 20
+ */
+
+    }
+
+}
