1c09bb86dec3609373b009637704bb014c1c5127
DocB <bverns88@gmail.com>
2023-11-18 10:24:34 -0800

    Day04_SwapVariables1 final

--- src/day04_concatenation/SwapTowVariables1.java
+++ src/day04_concatenation/SwapTowVariables1.java
@@ -6,8 +6,24 @@ public class SwapTowVariables1 {
 
             int x = 10;
             int y = 15;
+            int z = x;
+
+            x = y;
+            y = z;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
 
 
+        int h = 50;
+        int i = 40;
+        int j = h;
+            //
+        h = i;
+        i = j;
+
+        System.out.println("h = " + h);
+        System.out.println("i = " + i);
 
 
     }
@@ -19,7 +35,7 @@ Create a class named SwapTwoVariables1 and declare the following variables:
             x
             y
             z
-    8.1 Write a program that swap variables x & y’ values by using a temporary variable z
+    8.1 Write a program that swap variables x & y values by using a temporary variable z
         Ex:
               x = 10
               y = 15
