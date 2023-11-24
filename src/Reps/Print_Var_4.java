3a8aca3e0cdb9e16c0f4405d4828da06f563f406
Docrean <bverns88@gmail.com>
2023-11-24 15:31:42 -0800

    Print_Var_4 final

---
+++ src/Reps/Print_Var_4.java
@@ -0,0 +1,47 @@
+package Reps;
+
+public class Print_Var_4 {
+
+    public static void main(String[] args) {
+
+        String code = "Java",
+                food = "Apple",
+                test = "Testing",
+                sdet = "SDET";
+
+
+        System.out.println("Today's word of the day is: " + code);
+        System.out.println("Today's word of the day is: " + food);
+        System.out.println("Today's word of the day is: " + test);
+        System.out.println("Today's word of the day is: " + sdet);
+
+
+    }
+
+
+}
+
+/*
+### Given a String variable **`word`** that has some value (from Scanner), use the variable to display the word of the day.
+
+**Note:** You don't need to do anything with the Scanner.  Use the variable as done before.
+
+Main topics: print statement, variables, String, concatenation, Scanner
+
+```
+Ex:
+  Input:
+    Java
+
+  Output:
+    Today's word of the day is: Java
+```
+```
+Ex:
+  Input:
+    Apple
+
+  Output:
+    Today's word of the day is: Apple
+```
+*/
