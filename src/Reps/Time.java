0b739c5e52abb2a1f8ffc26943dc329289833a3a
Docrean <bverns88@gmail.com>
2023-11-25 00:12:20 -0800

    Time rough_drect

---
+++ src/Reps/Time.java
@@ -0,0 +1,34 @@
+package Reps;
+
+public class Time {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
+
+/*
+*  ### In this task, you will need to use multiple variables and combine them together into the time of the day.
+```
+  For example it will be something like this:
+    12:24:33 PM
+```
+
+
+1. In the beginning of the code, declare three `int` variables: `hour, minute, second` and declare a `String` variable: `amOrPm`.
+> You only need to declare, no assignment.
+
+2. In the middle of the code, the variables will be assigned a value using Scanner.
+> Do not touch anything in this section.
+
+3. In the end of the code, use the variables to put together and print the time of the day using the following format:
+
+  ```
+  hours:minutes:seconds amOrPm
+  ```
+*
+* */
+
