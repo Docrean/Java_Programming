c0151051a31bace68b90442ee15f227d7cc51632
Docrean <bverns88@gmail.com>
2023-11-25 11:07:18 -0800

    Print_Var_7Final

---
+++ src/Variable_Scanner_REPLITS/Print_Var_7.java
@@ -0,0 +1,38 @@
+package Variable_Scanner_REPLITS;
+
+public class Print_Var_7 {
+
+    public static void main(String[] args) {
+
+        String topic1 = "Java",
+                topic2 = "SQL";
+
+
+
+        System.out.println("I will learn \"" + topic1 + "\" and \"" + topic2 + "\" at Cydeo");
+
+    }
+
+}
+
+/*
+* ## Given two String variables **`topic1`** and **`topic2`** with values (from Scanner), write a statement to display the topics we will learn as a full sentence:
+
+```
+I will learn $topic1 and $topic2 at Cydeo.
+```
+
+**Note:** You don't need to do anything with the Scanner.  Use the variable as done before.
+
+Main topics: print statement, variables, String, concatenation, Scanner, escape characters
+
+```
+Ex:
+
+  Input:
+    Java
+    SQL
+
+  Output:
+    I will learn "Java" and "SQL" at Cydeo.
+* */
