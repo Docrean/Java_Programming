f22920086e8ea4dc5c970d9915519ef0b505dfaf
Docrean <bverns88@gmail.com>
2023-11-24 17:34:06 -0800

    Print_Var_5 final

---
+++ src/Reps/Print_Var_5.java
@@ -0,0 +1,46 @@
+package Reps;
+
+public class Print_Var_5 {
+
+    public static void main(String[] args) {
+
+        int ferst = 55,
+                sec = 123,
+                farst = -124,
+                secon = -500,
+                furst = 923,
+                seconds = 33223;
+
+        System.out.println("first is " + ferst + " and second is " + sec);
+        System.out.println("first is " + farst + " and second is " + secon);
+        System.out.println("first is " + furst + " and second is " + seconds);
+
+    }
+}
+
+/*
+### Given two int variables **`first`** and **`second`** with values (from Scanner), write a statement that will print the values in a single line. See the example below.
+
+**Note:** You don't need to do anything with the Scanner.  Use the variable as done before.
+
+Main topics: print statement, variables, primitive types, concatenation, Scanner
+
+```
+Ex:
+  Input:
+    55
+    123
+
+  Output:
+    first is 55 and second is 123
+```
+```
+Ex:
+  Input:
+    -124
+    -500
+
+  Output:
+    first is -124 and second is -500
+```
+*/
