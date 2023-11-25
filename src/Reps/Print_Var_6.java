2d6ab17e5005784aa6a83c83ddb8747ec1617d9f
Docrean <bverns88@gmail.com>
2023-11-24 21:06:54 -0800

    Print_Var_6 final

---
+++ src/Reps/Print_Var_6.java
@@ -0,0 +1,31 @@
+package Reps;
+
+public class Print_Var_6 {
+
+    public static void main(String[] args) {
+
+        int num = 2;
+        double cost = 5;
+
+        System.out.println("num = " + num);
+        System.out.println("cost = " + cost);
+    }
+}
+
+/*
+### Given two variables **`num`** and **`cost`** with values (from Scanner), write a statement to print those values following the example below.
+
+**Note:** You don't need to do anything with the Scanner.  Use the variable as done before.
+
+Main topics: print statement, variables, primitive types, concatenation, Scanner
+
+```
+Ex:
+  Input:
+    2
+    5
+
+  Output:
+    num = 2
+    cost = 5.0
+*/
