bfedb1252f4b3b08ba3cf6958e41e48140c03c9a
Docrean <bverns88@gmail.com>
2023-11-27 15:15:40 -0800

    IfAndElsePracticeUPDATE

--- src/day06_ifStatements/IfAndElsePractice.java
+++ src/day06_ifStatements/IfAndElsePractice.java
@@ -6,12 +6,30 @@ public class IfAndElsePractice {
 
         int score = 35;
 
-        if(score >= 60){
-            System.out.println("Passed");
-        }else{
+        if (score >= 60) {
             System.out.println("Failed");
-
+        } else {
+            System.out.println("Passed");
         }
 
+            int testScore = 50;
+
+            if (testScore >= 50) {
+                System.out.println("Passed");
+            } else {
+                System.out.println("Failed");
+            }
+
+            int examScore = 50;
+                //FIRST CONDITION MUST BE TRUE FOR COMPILE EXECUTION.
+                //IF FIRST CONDITION IS FALSE IT WON'T RUN.
+            if (examScore >=40) {
+                System.out.println("Passed");
+
+            if (examScore <=40) {
+                System.out.println("Failed");
+            }
+            }
+
+        }
     }
-}
