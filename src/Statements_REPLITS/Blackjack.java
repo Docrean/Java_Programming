3b29f7ac8c6003632b99ee6cc340454e995186fc
DocB <bverns88@gmail.com>
2023-12-01 15:02:56 -0800

    BlackjackFINALw/Notes

---
+++ src/Statements_REPLITS/Blackjack.java
@@ -0,0 +1,58 @@
+package Statements_REPLITS;
+
+public class Blackjack {
+
+    public static void main(String[] args) {
+
+        /*
+        * import java.util.*;
+class Main {
+  public static void main(String[] args) {
+     //DO NOT TOUCH BELOW
+    Scanner s = new Scanner(System.in);
+
+    int house = s.nextInt();
+    int player = s.nextInt();
+
+    //WRITE YOUR CODE BELOW
+
+    //Single Ifs
+    //if (player > 21){
+    //System.out.println("player bust");
+  //}
+
+    //if (player == house){
+    //System.out.println("its a tie");
+  //}
+
+    //if (player == 21){
+    //System.out.println("player win");
+  //}
+
+    //if (house > player){
+    //System.out.println("player lose");
+  //}
+
+    //if (player > house){
+    //System.out.println("player win");
+  //}
+
+    //Multi branch
+    if (player > 21){
+      System.out.println("player bust");
+    } else if (player == house){
+      System.out.println("its a tie");
+    } else if (player == 21){
+      System.out.println("player win");
+    } else if (house > player){
+      System.out.println("player lose");
+    } else if (player > house){
+      System.out.println("player win");
+    }
+
+        ////if no aforementioned result based on those conditions, for instance, if players dies, than single        else at end.
+    //that is why the single else at the end defines ultimate end condition if all previous previous conditions fail..
+    //Single if false will not run the code...will run next condition.
+    */
+    }
+}
