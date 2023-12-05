d99b3ab51729a7af831c0da400bd53588bc00692
Docrean <bverns88@gmail.com>
2023-12-04 17:01:11 -0800

    GradeLevelsFINAL

---
+++ src/Statements_REPLITS/GradeLevels.java
@@ -0,0 +1,81 @@
+package Statements_REPLITS;
+
+public class GradeLevels {
+
+    public static void main(String[] args) {
+
+        /*
+        * import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    //DO NOT TOUCH BELOW
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter age:");
+    int age = input.nextInt();
+
+
+    //WRITE YOUR CODE BELOW
+
+    String result;
+
+    if(age < 3){
+      result = "ineligible";
+    } else if (age <= 4){
+      result = "preschool";
+    } else if (age == 5){
+      result = "kindergarten";
+    } else if (age <= 10){
+      result = "elementary school";
+    } else if (age <= 13){
+      result = "middle school";
+    } else if (age <= 18){
+      result = "high school";
+    } else {
+      result = "college";
+    }
+
+    System.out.println(result);
+
+      //dont need to write age ranges...just specify the end value to compile
+  }
+}
+
+            * ### Use `if statements` to help the student find out which grade level they will join based on their `age`. Use the following information to determine the results:
+
+```
+less than 3   -> ineligible
+3-4           -> preschool
+5             -> kindergarten
+6-10          -> elementary school //refer to end value instead of ranges //dont need to write age ranges...just specify the end value to compile and write the correct expression...or else will have to       concat
+11-13         -> middle school
+14-18         -> high school
+19+           -> college
+```
+
+#### Hint: Use a multi branch if statement
+
+Main topics: if statements, primitive variables, operators
+
+```
+Ex:
+  Input:
+    8
+
+  Output:
+    elementary
+```
+```
+Ex:
+  Input:
+    5
+
+  Output:
+    kindergarten
+```
+            *
+        * */
+    }
+}
