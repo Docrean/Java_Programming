bf58ab25f741f0291a4dd1cd6f6a10cfe6bff84e
Docrean <bverns88@gmail.com>
2023-12-27 20:41:58 -0800

    PrintlnPrint

---
+++ src/day01_IntroToProgramming/PrintlnPrint.java
@@ -0,0 +1,26 @@
+package day01_IntroToProgramming;
+
+public class PrintlnPrint {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.println("-----------------------");
+
+        System.out.print("Hello");
+        System.out.print("World");
+
+        System.out.println();
+        System.out.println("---------------------------");
+
+        System.out.print("Hello");
+        System.out.println("Cydeo");
+        System.out.print("I Love ");
+        System.out.println("Java Programming");
+
+        System.err.println("Hello Cydeo");
+
+    }
+}
