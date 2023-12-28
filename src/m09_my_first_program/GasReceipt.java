065fdafc2f4bf3779a75525b4e92d106c41dd02f
Docrean <bverns88@gmail.com>
2023-12-28 00:53:17 -0800

    GasReceipt&UserInfoFINAL

---
+++ src/m09_my_first_program/GasReceipt.java
@@ -0,0 +1,15 @@
+package m09_my_first_program;
+
+public class GasReceipt {
+
+    public static void main(String[] args) {
+
+        System.out.println(         "\tMCLEAN STORE"     );
+        System.out.println(    "2019-06-19     04:38PM");
+        System.out.println(    "Gallons:       10.870");
+        System.out.println(    "Price/gallon:  $2.089");
+        System.out.println(    "Fuel total:    $22.71");
+
+
+    }
+}
---
+++ src/m09_my_first_program/UserInfo.java
@@ -0,0 +1,16 @@
+package m09_my_first_program;
+
+public class UserInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Name: Mike");
+        System.out.println("Last Name: Smith");
+        System.out.println();
+        System.out.println("Address: 100 Gallows Rd");
+        System.out.println("City: Fairfax");
+        System.out.println("State: VA");
+        System.out.println("Country: USA");
+
+    }
+}
