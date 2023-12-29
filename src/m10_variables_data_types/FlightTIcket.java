7c6f6eba4802d2162cb6a5d1858cda2a013ddb9d
Docrean <bverns88@gmail.com>
2023-12-29 15:33:25 -0800

    FlightTIcketFinal

---
+++ src/m10_variables_data_types/FlightTIcket.java
@@ -0,0 +1,15 @@
+package m10_variables_data_types;
+
+public class FlightTIcket {
+
+    public static void main(String[] args) {
+
+        String from = "London",
+                to = "Washington DC";
+
+        double ticketprice = 200;
+
+        System.out.println("From " + from + "to " + to + "is $" + ticketprice);
+
+    }
+}
