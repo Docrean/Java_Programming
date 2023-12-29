5cb701eafd9e0b0d7b3c810984cce2880032e257
Docrean <bverns88@gmail.com>
2023-12-29 14:21:02 -0800

    WholeNumbers&FloatingPointsUPDATED

---
+++ src/m10_variables_data_types/FLoatingPoints.java
@@ -0,0 +1,11 @@
+package m10_variables_data_types;
+
+public class FLoatingPoints {
+
+    public static void main(String[] args) {
+
+        float maxFloat = 3.14F;//compile error since default is double..apply f or F at end
+
+        System.out.println(maxFloat);
+    }
+}
