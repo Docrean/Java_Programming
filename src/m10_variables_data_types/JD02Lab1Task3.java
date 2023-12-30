561dc2f53d4e709ae91d884002962ff9148ea7dc
Docrean <bverns88@gmail.com>
2023-12-29 19:26:48 -0800

    JavaDay2TasksFINAL

---
+++ src/m10_variables_data_types/JD02Lab1Task3.java
@@ -0,0 +1,13 @@
+package m10_variables_data_types;
+
+public class JD02Lab1Task3 {
+
+    public static void main(String[] args) {
+
+        String names;
+
+        names = "Harold, " + "Mike, " + "Jessica, " + "Sam, " + "John";
+
+        System.out.println(names);
+    }
+}
