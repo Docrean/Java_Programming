74280c86bff310b8ab8e307eafd4bfa1307b3839
Docrean <bverns88@gmail.com>
2024-01-01 12:08:28 -0800

    Week02ReviewUpdateVariablesPracticeFINAL

---
+++ src/m11_operators_part1/Addition_VS_Concatenation.java
@@ -0,0 +1,12 @@
+package m11_operators_part1;
+
+public class Addition_VS_Concatenation {
+    public static void main(String[] args) {
+
+        System.out.println(3 + 5); //8--console adds together to print 8
+
+        System.out.println(3 + 5 + "Cydeo" + 3 + 5);
+
+
+    }
+}
---
+++ src/m11_operators_part1/CelsiusToFahrenheit.java
@@ -0,0 +1,17 @@
+package m11_operators_part1;
+
+public class CelsiusToFahrenheit {
+
+    public static void main(String[] args) {
+
+
+        double celsius = 26.8;
+        double fahrenheit;
+
+        fahrenheit =(celsius *1.8)+32;
+
+        System.out.println("fahrenheit = " + fahrenheit);
+    }
+
+}
+
