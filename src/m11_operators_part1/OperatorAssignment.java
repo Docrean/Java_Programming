28fb9ad24f87e4d912f0ec97168c52fc19d5499c
Docrean <bverns88@gmail.com>
2024-01-01 18:23:48 -0800

    ArithematicOperators& OperatorAssignmentFINAL

---
+++ src/m11_operators_part1/OperatorAssignment.java
@@ -0,0 +1,16 @@
+package m11_operators_part1;
+
+public class OperatorAssignment {
+
+    public static void main(String[] args) {
+
+        int chairs = 40; //declaration and initialization
+        System.out.println(chairs + 2);//42 just printing NOT assigned
+        int moreChairs = chairs + 5;//add 5 to chairs' value and assign it to moreChairs
+        System.out.println(moreChairs); //45. arithmetic operators don't change variable value
+        //only time variable value changes is if reassigned
+
+
+        //Applying arithmetic operation alone won't change the value of the variable
+    }
+}
