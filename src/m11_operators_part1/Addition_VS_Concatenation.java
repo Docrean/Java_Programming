7d84c84ba76773030fe9eb53caa2ae2611ed19ac
Docrean <bverns88@gmail.com>
2024-01-01 19:15:14 -0800

    Addition_VS_ConcatenationFINAL

--- src/m11_operators_part1/Addition_VS_Concatenation.java
+++ src/m11_operators_part1/Addition_VS_Concatenation.java
@@ -6,7 +6,19 @@ public class Addition_VS_Concatenation {
         System.out.println(3 + 5); //8--console adds together to print 8
 
         System.out.println(3 + 5 + "Cydeo" + 3 + 5);
+        //                  8 + "Cydeo" + 3 + 5
+        //                  "8Cydeo" + 3 + 5
+        //                  "8Cydeo3" + 5
+        //                  "8Cydeo35"
 
+        //from left to right ALWAYS
+
+        System.out.println(3 + 5 + "Cydeo" + (3 + 5));
+        //                  3 + 5 + "Cydeo" + 8
+        //                      8 + "Cydeo" + 8
+        //                         "8Cydeo" +8
+        //                         "8Cydeo8"
 
     }
+
 }
