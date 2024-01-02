05cc80a6733fd87c04588f4f3280856d168b9292
DocBFxClass <bverns88@gmail.com>
2024-01-02 09:53:47 -0800

    JD03labTasks1&2FINAL

--- src/m11_operators_part1/JD03Lab1Task1SwapValues.java
+++ src/m11_operators_part1/JD03Lab1Task1SwapValues.java
@@ -7,12 +7,18 @@ public class JD03Lab1Task1SwapValues {
 
         int num1 = 5, num2 = 10, num3;
 
-        System.out.println(num1);
-        System.out.println(num2);
+        //System.out.println(num1);
+       // System.out.println(num2);
 
-        System.out.println(num3 = num2);
-        System.out.println(num2 = num1);
+        //System.out.println(num3 = num2);
+       // System.out.println(num2 = num1);
 
+        num3 = num1;
+        num1 = num2;
+        num2 = num3;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
 
 
 
---
+++ src/m11_operators_part1/JD03Lab1Task2SwapValues.java
@@ -0,0 +1,32 @@
+package m11_operators_part1;
+
+public class JD03Lab1Task2SwapValues {
+
+    public static void main(String[] args) {
+
+        //int num1 = 5, num2 = 10;
+
+       // num1 = num1 + num2;
+        //num2 = num1 - num2;
+       // num1 = num1 - num2;
+
+       // System.out.println("num1 = " + num1);
+        //System.out.println("num2 = " + num2);
+
+
+        int num1 = 10, num2 = 15;
+
+        num1 = num1 + num2; //25
+        num2 = num1 - num2; //10. num2 now swapped in this step. need num1 as num2 still
+        num1 = num1 - num2; //15. num1 now swapped in this step to num2
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+
+
+
+
+
+    }
+}
