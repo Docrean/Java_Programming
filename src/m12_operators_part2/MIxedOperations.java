0fe94742097ed8d06ac5915cc65e1c7dd9285a8d
Docrean <bverns88@gmail.com>
2024-01-02 19:19:53 -0800

    MIxedOperationsUPDATE

---
+++ src/m12_operators_part2/MIxedOperations.java
@@ -0,0 +1,30 @@
+package m12_operators_part2;
+
+public class MIxedOperations {
+
+    public static void main(String[] args) {
+
+        byte num1 = 100;
+        short num2 = 20;
+
+        short num3 = (short) (num1 + num2);
+
+        int a = 100;
+        long b = 200;
+
+        long c = a + b;
+
+        float f = 20;
+        double g = 30;
+
+        double j = f + g;
+
+        long num9 = 200;
+        int num10 = 2;
+
+        long l = (int) num10 + num9;
+
+
+
+    }
+}
