37c4d8cd59aeb377bf2119403b36e9a8f431110d
Docrean <bverns88@gmail.com>
2024-01-02 17:04:36 -0800

    ImplicitExplicitCastingFINAL

--- src/m11_operators_part1/ExplicitCasting.java
+++ src/m11_operators_part1/ExplicitCasting.java
@@ -16,5 +16,28 @@ public class ExplicitCasting {
 
         System.out.println("num3 = " + num3); //num3 = 500
         System.out.println("num4 = " + num4); //num4 = -12. Data loss risk since out of smaller prims(bytes) range.k
+
+        long num5;
+        byte num6 = 52;
+
+        num5 =(long) num6;
+
+        System.out.println("num5 = " + num5);
+
+        short num7 = 39;
+        double num8;
+
+        num8 = num7;
+
+        System.out.println("num8 = " + num8);
+        System.out.println("num7 = " + num7);
+
+        float num9 = 12;
+        byte num10;
+
+        num10 =(byte) num9;
+
+        System.out.println("num10 = " + num10);
+        System.out.println("num9 = " + num9);
     }
 }
