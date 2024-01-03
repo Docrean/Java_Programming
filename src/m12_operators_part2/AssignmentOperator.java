05426562352e826ddb14e865d4839ef8bf4e543d
DocBFxClass <bverns88@gmail.com>
2024-01-02 21:34:50 -0800

    AssignmentOperatorUPDATE

---
+++ src/m12_operators_part2/AssignmentOperator.java
@@ -0,0 +1,17 @@
+package m12_operators_part2;
+
+public class AssignmentOperator {
+
+    public static void main(String[] args) {
+
+
+        int x = 15;
+
+        System.out.println(x); //assign int variable to x to get 15.
+
+        x = 20;
+
+        System.out.println("x = " + x);
+
+    }
+}
