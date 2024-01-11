e15c588c2725dcf0f99de9eaa10f42d145e8bf68
DocBFxClass <bverns88@gmail.com>
2024-01-10 22:11:32 -0800

    ForLoopUPDATE

--- src/m17_loops_part2/ForLoopRules.java
+++ src/m17_loops_part2/ForLoopRules.java
@@ -4,6 +4,8 @@ public class ForLoopRules {
 
     public static void main(String[] args) {
 
+        //     FOR LOOP RULES
+
         /*/ 1. Avoid infinite loops
         //If initialization and condition not specified loop runs indefinitely
         //even with two semicolons given
@@ -48,12 +50,35 @@ public class ForLoopRules {
                 to increment j by 10 rather than by 1 (j++), use the add assign operator
                 +=. As many variables can be assigned in the initializer, however, they
                 must be the same data type or else compiler error. In this case, must
-                be an int.
+                be an int. If wanted more boolean conditions for j && k than can specify
+                but for now loop will be executed only 10x as i < 11(exclusive) indicates.
+                Read the loop: i's value starts from 1 and ends at 10 (< 11). j starts from
+                10 and k starts from 100 and both will stop when i runs from 1 (i = 1) --> i < 11.
+                j will increase by 10 and k increase by 100.
                  */
+
+
             for (int i = 1, j = 10, k = 100; i < 11; i++, j += 10, k += 100) {
-                System.out.println(i + " | " + j);
+                System.out.println(i + " | " + j + " | " + k);
         }
-        
-        
-    }
+        System.out.println("----------------------------------------");
+
+            // 3. re-assigning a variable in the for loop block
+                int x;
+        for (x = 0; x <= 10 ; x++) {
+            System.out.println(x);
+        }
+
+        System.out.println("----------------------------------------");
+
+            // 4. loop variable cannot be used outside the for loop's body
+                    //declaring the variable in the initialization sets the for loops body
+                    //it cannot be used outside the for loops body or else compiler error
+                    //if tried to access from outside the body.
+        for (int i = 10; i <= 120; i+= 20) {
+            System.out.println(i);
+        }
+
+       // System.out.println(i); compiler error. cannot initialize outside the for loop body
+    }                   //   ^
 }
