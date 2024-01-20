4effb51d6fed2ad15c0bca92e223e05c2af350c8
Docrean <bverns88@gmail.com>
2024-01-20 09:10:47 -0800

    ForLoopRulesUPDATE

---
+++ src/m25_class_and_object/Dog.java
@@ -0,0 +1,25 @@
+package m25_class_and_object;
+
+public class Dog {
+
+
+
+
+}
+
+        /*
+        Create a "Dog" class and define the below instance fields and methods
+
+        Fields:
+        String bread
+        int age
+        String color
+        String name
+
+        Methods:
+        bark()
+        eat()
+        sleep()
+
+
+         */
