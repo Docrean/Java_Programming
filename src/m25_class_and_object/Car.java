14341f761cb7cf10a0158c4f559623d6fa4f7e01
Docrean <bverns88@gmail.com>
2024-01-23 18:11:56 -0800

    m25UpdAte

--- src/m25_class_and_object/Car.java
+++ src/m25_class_and_object/Car.java
@@ -3,6 +3,22 @@ package m25_class_and_object;
 public class Car {
 
 
+
+    public String make, model;
+    public int year;
+    public String color;
+    public double price;
+    public void start(){
+        System.out.println(make + " " + model + " is starting");
+    }
+    public void drive(){
+        System.out.println(make + " " + model + " is driving");
+    }
+    public void print(){
+        System.out.println(make + " " + model + " is stopping");
+    }
+
+    public void showCurrentSpeed(int currentSpeed, int speedLimit){}
 }
 
 
---
+++ src/m25_class_and_object/CarClient.java
@@ -0,0 +1,4 @@
+package m25_class_and_object;
+
+public class CarClient {
+}
