09db347482a9148a72d78e0dfa89287ee2814404
Docrean <bverns88@gmail.com>
2024-01-27 21:19:15 -0800

    CarFINAL

---
+++ src/m26_user_input_scanner/ScannerIntegers.java
@@ -0,0 +1,12 @@
+package m26_user_input_scanner;
+
+    import java.util.Scanner; //can't create scanner object without importing
+                                //use util.scanner hotkey
+public class ScannerIntegers {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); //use in variable as input string
+                                                //System.in used for scanner object getting input during runtime
+    }
+}
