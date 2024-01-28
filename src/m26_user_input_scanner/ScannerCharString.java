993bc5d2cc198bee1f439c8fff76e20b30da24ff
Docrean <bverns88@gmail.com>
2024-01-28 10:10:41 -0800

    ScannerCharString

--- src/m26_user_input_scanner/ScannerCharString.java
+++ src/m26_user_input_scanner/ScannerCharString.java
@@ -1,8 +1,15 @@
 package m26_user_input_scanner;
 
+import java.util.Scanner;
+
 public class ScannerCharString {
 
     public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a character:");
 
 
     }
