99a53de3608cbfb5a8ca82ad4af88e8bc1335d38
Docrean <bverns88@gmail.com>
2024-01-28 10:06:14 -0800

    ScannerCharStringUPDATE

---
+++ src/m26_user_input_scanner/ScannerCharString.java
@@ -0,0 +1,26 @@
+package m26_user_input_scanner;
+
+public class ScannerCharString {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+        /*
+        Write a program that asks the user to enter a character and then a question
+        "Would you like to continue?". It should print inputs as shown on the output.
+
+            Example:
+                Input: '*', Yes
+                Output:
+
+                   Enter a character:
+                   *
+                   You have entered: *
+                   Would you like to continue?
+                   Yes
+                   You have entered: Yes
+         */
--- src/m26_user_input_scanner/ScannerDecimals.java
+++ src/m26_user_input_scanner/ScannerDecimals.java
@@ -19,6 +19,24 @@ public class ScannerDecimals {
         boolean r = input.nextBoolean(); //boolean
 
         input.close();
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("r = " + r);
     }
 
 }
+
+
+        /*
+        Write a program that asks the user to enter two decimal numbers and a "true/false" question. Then print user
+        inputs as shown on the output.
+
+            Example:
+                Input: 1.1, 10.2, true
+                Output:
+
+                    num1 = 1.1
+                    num2 = 10.2
+                    r = true
+         */
