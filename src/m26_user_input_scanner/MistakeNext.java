80df8c831a7d3e18141bc86c7d6eade044d39b1f
Docrean <bverns88@gmail.com>
2024-01-28 16:06:55 -0800

    MistakeNext

---
+++ src/m26_user_input_scanner/MistakeNext.java
@@ -0,0 +1,15 @@
+package m26_user_input_scanner;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MistakeNext {
+
+    public static void main(String[] args) {
+
+
+    Scanner scanMe = new Scanner(System.in);
+    String firstName, lastName;
+
+    }
+}
