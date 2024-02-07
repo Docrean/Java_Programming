5a086769db16d81c779b8d811208353650cc201c
Docrean <bverns88@gmail.com>
2024-02-06 16:27:34 -0800

    JavaLangUPDATE

---
+++ src/m35_java_lang_classes/MathClass.java
@@ -0,0 +1,22 @@
+package m35_java_lang_classes;
+
+public class MathClass {
+
+    public static void main(String[] args) {
+
+       int num1 = Math.max(100, 200); //Math. keyword
+        System.out.println(num1);
+
+       int num2 = Math.min(28, 128);
+        System.out.println(num2);
+
+
+        System.out.println(Math.abs(-120));
+        //absolute value is always positive
+        //returns 120 and can include statement in the print statement.
+
+        int num4 = Math.pow(10, 2);
+
+
+    }
+}
