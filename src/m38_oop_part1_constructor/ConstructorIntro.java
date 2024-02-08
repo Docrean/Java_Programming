8cb7efe2775c8c9dc1d8bb8e60a3045e6c1324a4
Docrean <bverns88@gmail.com>
2024-02-08 15:52:31 -0800

    ConstructorIntroUPDATE

---
+++ src/m38_oop_part1_constructor/ConstructorIntro.java
@@ -0,0 +1,22 @@
+package m38_oop_part1_constructor;
+
+public class ConstructorIntro { //make sure constructor name is same as class name
+
+    //Create constructor starting with access modifier (done manually)
+    //cannot give
+
+    public static void main(String[] args) { //create this main method if not using a custom class
+        //write some codes and run them in this class
+        //To create object from ConstructorIntro class first create the reference variable name ConstructorIntro
+        //Use the new keyword.
+        //Use class name with ()
+        //without creating any constructor the compiler will create its own by default
+        //EVERY SINGLE CLASS MUST HAVE A CONSTRUCTOR...then instantiate the class with = new ConstructorIntro() object
+
+
+        ConstructorIntro obj = new ConstructorIntro();; //default constructor
+
+
+
+    }
+}
