5d354c0915fb4abc4b75c408534816872d9f57be
Docrean <bverns88@gmail.com>
2024-02-08 17:21:59 -0800

    ConstructorIntroUPDATE

--- src/m38_oop_part1_constructor/ConstructorIntro.java
+++ src/m38_oop_part1_constructor/ConstructorIntro.java
@@ -2,8 +2,12 @@ package m38_oop_part1_constructor;
 
 public class ConstructorIntro { //make sure constructor name is same as class name
 
-    //Create constructor starting with access modifier (done manually)
-    //cannot give
+    //Create constructor starting with access modifier(public) (done manually)
+    //cannot give any static modifier(static) nor return types(void main)
+    //Constructor name has to be the same as the class name
+    public ConstructorIntro() { //no argument constructor means compiler will not give a default constructor
+        System.out.println("Object is created by using no-arg constructor"); //this prints via the no argument constructor
+    }
 
     public static void main(String[] args) { //create this main method if not using a custom class
         //write some codes and run them in this class
@@ -14,7 +18,9 @@ public class ConstructorIntro { //make sure constructor name is same as class na
         //EVERY SINGLE CLASS MUST HAVE A CONSTRUCTOR...then instantiate the class with = new ConstructorIntro() object
 
 
-        ConstructorIntro obj = new ConstructorIntro();; //default constructor
+        ConstructorIntro obj = new ConstructorIntro(); //class object(instance). have to use a constructor that exists in that class(no argument constructor in this case)
+                                                        //this is why can use the no argument constructor to create the new ConstructorIntro() object
+                                                        //if the constructor passes an argument than the object will show compile error
 
 
 
