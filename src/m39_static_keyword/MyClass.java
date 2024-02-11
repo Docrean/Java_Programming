92fc8b7f4a787b45a5f5b2e1ba49936ae417b3eb
Docrean <bverns88@gmail.com>
2024-02-10 19:28:29 -0800

    StaticKeywordUPDATE

---
+++ src/m39_static_keyword/MyClass.java
@@ -0,0 +1,13 @@
+package m39_static_keyword;
+
+public class MyClass {
+
+    public static int staticVar;
+    public int instanceVar;
+
+    public static void main(String[] args) {
+
+        System.out.println(staticVar);
+        System.out.println(instanceVar);
+    }
+}
