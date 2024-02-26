17a75ceb86ab051333823d6a20c98ca81d3a3fbb
Docrean <bverns88@gmail.com>
2024-02-26 10:48:18 -0800

    InheritancePt2UPDATE

---
+++ src/M42_access_modifiers_final_object_class/package1/ClassA.java
@@ -0,0 +1,9 @@
+package M42_access_modifiers_final_object_class.package1;
+
+public class ClassA {
+
+    public static int a = 100;
+    protected static int b = 200;
+    static int c = 300; //no keyword for default access modifier. default is used in an interface.
+    private int d = 400;
+}
---
+++ src/M42_access_modifiers_final_object_class/package1/ClassB.java
@@ -0,0 +1,13 @@
+package M42_access_modifiers_final_object_class.package1;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+        //ClassA variables are static so can call through here. Which access modifier(s) can be called in same package?
+
+        System.out.println(ClassA.a);
+        System.out.println(ClassA.b);
+        System.out.println(ClassA.c);
+        System.out.println(ClassA.d); //private not accessible in same package!
+    }
+}
