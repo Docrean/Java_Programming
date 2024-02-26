223db9f0f1fef952b57f960dfaa9111356972667
Docrean <bverns88@gmail.com>
2024-02-26 11:18:13 -0800

    InheritancePt2UPDATE

--- src/M42_access_modifiers_final_object_class/package1/ClassA.java
+++ src/M42_access_modifiers_final_object_class/package1/ClassA.java
@@ -7,3 +7,13 @@ public class ClassA {
     static int c = 300; //no keyword for default access modifier. default is used in an interface.
     private int d = 400;
 }
+/*
+Access Modifier     Description         Same Class      Same package    Sub-class       World
+public              everywhere            Yes             Yes             Yes           Yes
+protected     within same class/subclass  Yes             Yes             Yes            No
+default
+(Packageprivate)  within same package     Yes             Yes              No            No
+private           only within same class  Yes             No               No            No
+
+
+ */
