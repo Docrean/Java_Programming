a25614dd5a8f4b66a45f88098b152de60edce477
Docrean <bverns88@gmail.com>
2024-02-26 11:59:58 -0800

    InheritancePt2UPDATE

--- src/M42_access_modifiers_final_object_class/package1/ClassB.java
+++ src/M42_access_modifiers_final_object_class/package1/ClassB.java
@@ -8,6 +8,6 @@ public class ClassB {
         System.out.println(ClassA.a);
         System.out.println(ClassA.b);
         System.out.println(ClassA.c);
-        System.out.println(ClassA.d); //private not accessible in same package!
+        //System.out.println(ClassA.d); //private not accessible in same package!
     }
 }
