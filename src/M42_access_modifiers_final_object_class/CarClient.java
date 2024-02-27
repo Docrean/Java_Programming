9e67dcb9ef329c580d6aeb3e0e28765ff66ba91d
Docrean <bverns88@gmail.com>
2024-02-27 15:52:54 -0800

    InheritanceUPDATE

--- src/M42_access_modifiers_final_object_class/CarClient.java
+++ src/M42_access_modifiers_final_object_class/CarClient.java
@@ -1,12 +1,16 @@
 package M42_access_modifiers_final_object_class;
 
-import m25_class_and_object.Car;
 
 public class CarClient { //create the object of the CarObjectClass
 
     public static void main(String[] args) {
 
-        CarObjectClass car
+        CarObjectClass car = new CarObjectClass(); //compiler creates constructor by default if not manually generated
+
+        System.out.println(car);  //pass the variable car as reference variable
+        //car.finalize(); //car is unreferenced so it is garbage collected.
+        car = null; //object now eligible for garbage collection using null keyword(not
+                    //since eligible for garbage collection, garbage collector calls finalize method implicitly
     }
 
 
