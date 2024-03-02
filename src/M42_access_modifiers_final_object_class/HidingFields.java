019f2c3d5b67f1c08f3c2cce0fee09868bf1507e
DocBFxClass <bverns88@gmail.com>
2024-03-01 19:47:25 -0800

    InheritanceUPDATE

--- src/M42_access_modifiers_final_object_class/HidingFields.java
+++ src/M42_access_modifiers_final_object_class/HidingFields.java
@@ -10,6 +10,10 @@ class Parent{
 
             public static int a = 100; //this static int variable is now inherited to the child subclass..which means..
                                        //this static int variable can be called from the parent and child class
+            public int b = 200; //this instance variable is inherited as it is to the child class
+
+                                //How can we change the value of instance variable b? Define it in the child class and
+                                //assign the new value.
 
 
     public static void staticMethod(){ //REMOVING THE STATIC KEYWORD = METHOD OVERRIDING
@@ -28,6 +32,10 @@ class Child extends Parent{ //declare the staticMethod() in the child class(acce
                             //Since defining staticMethod()(is the method signature of its parent class public static void staticMethod(),
                             //when the parent class method is inherited to the child class, it will be hidden.
                             //So now, the child class can be re-written as "Child class' static method" without inheritance conflict.
+
+    public static int a = 1000; //Parent class static variable a is hidden to the child class. Now the new value of 1000
+                                //of the child class will print to console
+    public int b = 300; //parent instance variable b is now hidden in the child class since child class now defined b
 
     public static void staticMethod(){ //this is the static method defined in the child class.
                                         //REMOVING THE STATIC KEYWORD = METHOD OVERRIDING
@@ -48,9 +56,21 @@ public class HidingFields { //concept of hiding fields is similar to method hidi
 
         System.out.println(Parent.a);
         System.out.println(Child.a);
+
+        //static variables can be hidden in the child class as well as instance variables(see public in b example)
+
+
+        //How to create parent class objects from the instance variable b? set parent variable with new keyword:
+
+        Parent parent = new Parent();
+        Child child = new Child();
+
+        System.out.println(parent.b);
+        System.out.println(child.b);
+
     }
 
 
-
+        //This is field hiding: The child class calls the parent class fields in which parent and child will
 
 }
