6096ff9f44d04e9a10db39826c49bb4e1b3be387
DocBFxClass <bverns88@gmail.com>
2024-03-02 16:46:14 -0800

    UPDATE

--- src/m10_variables_data_types/BooleanVariable.java
+++ src/m10_variables_data_types/BooleanVariable.java
@@ -1,5 +1,7 @@
 package m10_variables_data_types;
 
+import java.util.Arrays;
+
 public class BooleanVariable {
     public static void main(String[] args){
 
@@ -10,5 +12,15 @@ public class BooleanVariable {
         boolean isEmployed = true;
 
         System.out.println(isEmployed);
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(arr.length);
+        System.out.println(arr[0]);
+
+        arr[0] = 100;
+        System.out.println(arr[0]);
+
+        System.out.println(Arrays.toString(arr));
+
     }
 }
