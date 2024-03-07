a39ffc3b750272f101dc0fbd7a24e6d4d77434cc
DocBFxClass <bverns88@gmail.com>
2024-03-07 04:18:10 -0800

    AbstractPolyMorphUpdate

---
+++ src/m43_oop_part4_abstraction/Cube.java
@@ -0,0 +1,9 @@
+package m43_oop_part4_abstraction;
+
+public class Cube {
+    //before extending the cube from the shape class, first include common variables of the cube.
+    //every single cube has a side.
+    //create an instance variable side for the cube.
+    //whenever the cube object is created, need to set the instance variable side...GENERATE THE CONSTRUCTOR..
+    public double side;
+}
